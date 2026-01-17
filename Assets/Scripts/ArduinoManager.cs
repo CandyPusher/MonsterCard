58ca5bb99265b09b25dc1f42cc50109761d7e77d
CandyPusher <spalmalupi@gmail.com>
2026-01-17 02:22:13 +0100

    si

--- Assets/Scripts/ArduinoManager.cs
+++ Assets/Scripts/ArduinoManager.cs
@@ -4,167 +4,200 @@ using UnityEngine;
 
 public class ArduinoManager : MonoBehaviour
 {
-    [Header("Connessione")]
-    [SerializeField] string PORT = "COM3";
-    [SerializeField] int BAUD = 9600;
-    [SerializeField] int readingSpeed = 50;
+	[Header("Connessione")]
+	[SerializeField] string PORT = "COM3";
+	[SerializeField] int BAUD = 9600;
+	[SerializeField] int readingSpeed = 50;
 
-    [Header("Riferimenti")]
-    [SerializeField] int playerIndex = 1;
+	[Header("Riferimenti")]
+	[SerializeField] int playerIndex = 1;
 
-    private SerialPort sp;
+	private SerialPort sp;
+	
+	public void DebugNoArduino(string _arduinoCtx)
+	{
+		string incomingData = _arduinoCtx.Trim();
 
-    private void Awake()
-    {
-        sp = new SerialPort(PORT, BAUD);
-        sp.ReadTimeout = readingSpeed;
-    }
+		if (incomingData == "Pressed")
+		{
+			GameManager.Instance.gameFlowManager.PlayerInteracted(playerIndex);
+			Debug.Log($"Input: {incomingData}");
+			return;
+		}
 
-    private void Start()
-    {
-        try { sp.Open(); }
-        catch (Exception e) { Debug.LogError("Errore porta seriale: " + e.Message); }
-    }
+		string[] parts = incomingData.Split('-');
 
-    private void Update()
-    {
-        if (sp == null || !sp.IsOpen) return;
+		if (parts.Length == 2)
+		{
+			if (int.TryParse(parts[0], out int cardID) && int.TryParse(parts[1], out int deckID))
+			{
+				HandleInput(cardID, deckID);
+			}
+		}
+	}
+	
+	public void DebugPressArduino()
+	{
+		GameManager.Instance.gameFlowManager.PlayerInteracted(playerIndex);
+	}
+	
+	private void Awake()
+	{
+		try
+		{
+			sp = new SerialPort(PORT, BAUD);
+			sp.ReadTimeout = readingSpeed;
+		} catch (Exception e)
+		{
+			Debug.LogError("Errore porta seriale: " + e.Message);
+		}
+	}
 
-        try
-        {
-            string incomingData = sp.ReadLine().Trim();
+	private void Start()
+	{
+		try { sp.Open(); }
+		catch (Exception e) { Debug.LogError("Errore porta seriale: " + e.Message); }
+	}
 
-            if (incomingData == "Pressed")
-            {
-                GameManager.Instance.gameFlowManager.PlayerInteracted(playerIndex);
-                Debug.Log($"Input: {incomingData}");
-                return;
-            }
+	private void Update()
+	{
+		if (sp == null || !sp.IsOpen) return;
 
-            string[] parts = incomingData.Split('-');
+		try
+		{
+			string incomingData = sp.ReadLine().Trim();
 
-            if (parts.Length == 2)
-            {
-                if (int.TryParse(parts[0], out int cardID) && int.TryParse(parts[1], out int deckID))
-                {
-                    HandleInput(cardID, deckID);
-                }
-            }
-        }
-        catch (TimeoutException) { }
-        catch (Exception e) { Debug.LogWarning("Errore lettura: " + e.Message); }
-    }
+			if (incomingData == "Pressed")
+			{
+				GameManager.Instance.gameFlowManager.PlayerInteracted(playerIndex);
+				Debug.Log($"Input: {incomingData}");
+				return;
+			}
 
-    private void HandleInput(int cardID, int deckID)
-    {
-        Debug.Log($"Carta: {cardID} | Deck: {deckID}");
+			string[] parts = incomingData.Split('-');
 
-        if (cardID == 0)
-        {
-            GameManager.Instance.gameFlowManager.setPlayerDeck(playerIndex, deckID);
-            return;
-        }
-        else
-        {
-            switch (deckID)
-            {
-                case 1:
-                    HandleFistDeck(cardID);
-                    break;
-                case 2:
-                    HandleSecondDeck(cardID);
-                    break;
-                case 3:
-                    HandleThirdDeck(cardID);
-                    break;
-            }
-            return;
-        }
-    }
+			if (parts.Length == 2)
+			{
+				if (int.TryParse(parts[0], out int cardID) && int.TryParse(parts[1], out int deckID))
+				{
+					HandleInput(cardID, deckID);
+				}
+			}
+		}
+		catch (TimeoutException) { }
+		catch (Exception e) { Debug.LogWarning("Errore lettura: " + e.Message); }
+	}
 
-    private void HandleFistDeck(int _cardID)
-    {
-        switch (_cardID)
-        {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-        }
-    }
+	private void HandleInput(int cardID, int deckID)
+	{
+		Debug.Log($"Carta: {cardID} | Deck: {deckID}");
 
-    private void HandleSecondDeck(int _cardID)
-    {
-        switch (_cardID)
-        {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-        }
-    }
+		if (cardID == 0)
+		{
+			GameManager.Instance.gameFlowManager.setPlayerDeck(playerIndex, deckID);
+			return;
+		}
+		else
+		{
+			switch (deckID)
+			{
+				case 1:
+					HandleFistDeck(cardID);
+					break;
+				case 2:
+					HandleSecondDeck(cardID);
+					break;
+				case 3:
+					HandleThirdDeck(cardID);
+					break;
+			}
+			return;
+		}
+	}
 
-    private void HandleThirdDeck(int _cardID)
-    {
-        switch (_cardID)
-        {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-        }
-    }
+	private void HandleFistDeck(int _cardID)
+	{
+		switch (_cardID)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case 9:
+				break;
+			case 10:
+				break;
+		}
+	}
 
-    private void OnApplicationQuit()
-    {
-        if (sp != null && sp.IsOpen) sp.Close();
-    }
+	private void HandleSecondDeck(int _cardID)
+	{
+		switch (_cardID)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case 9:
+				break;
+			case 10:
+				break;
+		}
+	}
+
+	private void HandleThirdDeck(int _cardID)
+	{
+		switch (_cardID)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case 9:
+				break;
+			case 10:
+				break;
+		}
+	}
+
+	private void OnApplicationQuit()
+	{
+		if (sp != null && sp.IsOpen) sp.Close();
+	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameFlowManager gameFlowManager;
-    public ArduinoManager arduinoManager_P1;
-    public ArduinoManager arduinoManager_P2;
-    public static GameManager Instance { get; private set; }
+	public GameFlowManager gameFlowManager;
+	public ArduinoManager arduinoManager_P1;
+	public ArduinoManager arduinoManager_P2;
+	public static GameManager Instance { get; private set; }
 
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Instance = this;
-        }
-    }
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+	}
+	
+	public void CheckIfPlayerIsReady()
+	{
+		if(gameFlowManager.player1_isReady && gameFlowManager.player2_isReady)
+		{
+			StartGame();
+		}
+	}
+	
+	public void StartGame()
+	{
+		
+	}
 }
